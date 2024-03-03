f94f8e810844842ed1de3dd8c127e235abeb3102
James Nicolas <jamesnicolas@me.com>
2024-03-03 13:52:46 -0500

    rebuild script

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -42,7 +42,13 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome = {
+      enable = true;
+      extraGSettingsOverrides = ''
+        [org.gnome.mutter]
+        center-new-windows = true;
+      '';
+  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
